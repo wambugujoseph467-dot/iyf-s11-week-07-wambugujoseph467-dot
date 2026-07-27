3bd0eac3945afe578d007ea531b724b4eea30248
Joseph Wambugu <wambugujoseph467@gmail.com>
2026-07-27 13:02:31 +0300

    Complete Task 13.3: Session Storage Form

---
+++ script3.js
@@ -0,0 +1,40 @@
+const form = document.getElementById("contact-form");
+
+const inputs = form.querySelectorAll("input, textarea");
+
+// Load saved values
+inputs.forEach(function(input){
+
+    const saved = sessionStorage.getItem("form_" + input.name);
+
+    if(saved){
+        input.value = saved;
+    }
+
+    input.addEventListener("input", function(){
+
+        sessionStorage.setItem(
+            "form_" + input.name,
+            input.value
+        );
+
+    });
+
+});
+
+// Submit
+form.addEventListener("submit", function(event){
+
+    event.preventDefault();
+
+    alert("Form submitted successfully!");
+
+    inputs.forEach(function(input){
+
+        sessionStorage.removeItem("form_" + input.name);
+
+    });
+
+    form.reset();
+
+});
